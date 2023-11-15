c4ed3d450b84d7b20741e9a42be6ca2e2c1339a7
smsirajk <69889801+smsirajk@users.noreply.github.com>
2023-11-16 00:16:25 +0530

    Create Python_Worksheets_1.sql

---
+++ procedure/Python_Worksheets_1.sql
@@ -0,0 +1,17 @@
+# The Snowpark package is required for Python Worksheets. 
+# You can add more packages by selecting them using the Packages control and then importing them.
+
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import col
+
+def main(session: snowpark.Session): 
+    # Your code goes here, inside the "main" handler.
+    tableName = 'my_db.information_schema.packages'
+    dataframe = session.table(tableName).filter(col("language") == 'python')
+
+    # Print a sample of the dataframe to standard output.
+    dataframe.show()
+
+    # Return value will appear in the Results tab.
+    return dataframe;
+    
